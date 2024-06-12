2bee4a428068871f0e7245e3772501b167967b0b
Raphael Sales <raphaelsales1337@gmail.com>
2024-06-11 23:24:37 -0300

    Correções

--- todo_project/tasks/serializers.py
+++ todo_project/tasks/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Task
-
+from tasks import models
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
